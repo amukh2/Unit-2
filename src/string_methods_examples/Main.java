5853f881ff8468facf35b218dd0ceeb1d28032c4
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-10-10 15:02:24 -0700

    update

--- src/string_methods_examples/Main.java
+++ src/string_methods_examples/Main.java
@@ -3,16 +3,25 @@ package string_methods_examples;
 public class Main {
     public static void main(String[] args) {
         //String with constructor vs string literal
+        String name = new String("Anshu");
+        String myName = "Anshu";
+        String anotherName = new String("Anshu");
+        String myAnotherName = "Anshu";
+
+        System.out.println(name == anotherName);
+        System.out.println(myName == myAnotherName);
 
         //String length
-
+        System.out.println(myName.length());
         //substring method
-
+        System.out.println(myName.substring(0,4));
         //indexOf
-
+        System.out.println(myName.indexOf("s"));
         //equals method
+        // to compare two objects, use .equals not ==, == id only for primitives
+        System.out.println(name.equals(anotherName));
 
         //compareTo
-
+        System.out.println(myName.compareTo(name));
     }
 }
